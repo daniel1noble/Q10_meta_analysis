559edc33c193b940bab6d3449c2de0d96f55dbef
Daniel Noble <daniel.noble@anu.edu.au>
2022-11-23 07:33:38 +1100

    added pmcmc function

--- R/functions.R
+++ R/functions.R
@@ -94,7 +94,12 @@ se_to_sd <- function(se, n){
 }
 
 
-
+## pMCMC Function
+# Calculates the, p-value or pMCMC value for a posterior distribution
+# x The vector for the posterior distribution. Note that this will test the null hypothesis that the parameter of interest is significantly different from 0. 
+pmcmc <- function(x){
+  2*(1 - max(table(x<0) / length(x)))
+}
 
 
 #Tree checks function
